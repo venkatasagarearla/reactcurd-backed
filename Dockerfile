9d42fd95cf8de5820940d6b3c5a854bdcb473d0c
venkatasagarearla <earlavenkatasagar@gmail.com>
2025-06-17 16:49:29 +0530

    added docker-compose and prometheus file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-FROM eclipse-temurin:17-jdk-focal
- 
-COPY target/*.jar app.jar
+# ---- Stage 1: Build ----
+FROM eclipse-temurin:17-jdk AS builder
+WORKDIR /app
+COPY . .
+RUN chmod +x mvnw
+RUN ./mvnw clean package -DskipTests -e -X
 
-ENTRYPOINT [ "java", "-jar", "app.jar" ]
+# ---- Stage 2: Run ----
+FROM eclipse-temurin:17-jre-alpine
+WORKDIR /app
+COPY --from=builder /app/target/*.jar app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/reactcurd/DemoApplicationTests.java
+++ src/test/java/com/reactcurd/DemoApplicationTests.java
@@ -1,40 +1,12 @@
 package com.reactcurd;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.reactcurd.entity.Employee;
-import com.reactcurd.repository.EmployeeRepository;
-import com.reactcurd.service.EmployeeService;
 
 
 
-@ExtendWith(MockitoExtension.class)
+
+
+
 class DemoApplicationTests {
 
-	    @InjectMocks
-		private EmployeeService empService;
-	
-		@Mock
-		private EmployeeRepository emprepo;
-		
-		@Test
-		public void getAllEmployees() {
-			when(emprepo.findAll()).thenReturn(Stream.of(new Employee(1L,"sagar","Earla","earla@gmail.com"),new Employee(2L,"venkata","earla","sagar@gmail.com")).collect(Collectors.toList()));
-			assertEquals(2,empService.getAllEmployees().size());
-		}
-	
+	    
 }
